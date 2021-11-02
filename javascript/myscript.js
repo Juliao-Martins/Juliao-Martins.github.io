2ef1c5f89945a53e23c6564a74557c410b82acc6
Juliao-Martins <juliaodejesusmartins@gmail.com>
2021-11-02 21:42:04 +0700

    interactive in information.

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,3 +1,19 @@
 const sInfo = document.getElementById('special-info');
 
-console.log(sInfo);
+const caption = {
+  first: 'Ami nia website ida nee sei falta husi buat barak karik kolega sira iha hanoin, sujestaun, ou kritika bele haruka karta liu husi ami nia email <a href="mailto:juliaodejesusmartins@gmail.com">juliaodejesusmartins@gmail.com</a>',
+  second: "Ami nia projetu ida nee <b><i>Open Source</i></b> karik ita <br> iha interese atu dezenvolve diak liu tan bele <br> foti ami nia <b>Source Code</b> iha <a href='https://github.com/Juliao-Martins/' target='_blank'>GitHub</a>."
+}
+
+let i = 0;
+window.setInterval(() => {
+  if(i === 0) {
+    sInfo.innerHTML = caption.second;
+    console.log(i);
+    i = 1;
+  } else {
+    sInfo.innerHTML = caption.first;
+    console.log(i);
+    i = 0;
+  }
+}, 10000);
