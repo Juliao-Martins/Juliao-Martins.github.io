c866923fdf6f26f93f8966b651041373c64bcc6b
Juliao-Martins <juliaodejesusmartins@gmail.com>
2021-11-03 19:39:30 +0700

    repair link to this repo.

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -4,7 +4,7 @@ const sInfo = document.getElementById('special-info');
 // declaration some object with const keyword variable
 const caption = {
   first: 'Ami nia website ida nee sei falta husi buat barak karik kolega sira iha hanoin, sujestaun, ou kritika bele haruka karta liu husi ami nia email <a href="mailto:juliaodejesusmartins@gmail.com">juliaodejesusmartins@gmail.com</a>',
-  second: "Ami nia projetu ida nee <b><i>Open Source</i></b>, karik kolega sira iha interese atu dezenvolve diak liu tan bele foti ami nia <b>Source Code</b> iha <a href='https://github.com/Juliao-Martins/' target='_blank'>GitHub</a>. Ami iha esperansa wainhira serbisu ida nee ida kolabora ho ema barak."
+  second: "Ami nia projetu ida nee <b><i>Open Source</i></b>, karik kolega sira iha interese atu dezenvolve diak liu tan bele foti ami nia <b>Source Code</b> iha <a href='https://github.com/Juliao-Martins/Juliao-Martins.github.io' target='_blank'>GitHub</a>. Ami iha esperansa wainhira serbisu ida nee ida kolabora ho ema barak."
 }
 
 // declartion variable i
